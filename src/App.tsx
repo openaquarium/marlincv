880291a9519ffe697c68cc1e91c2ff3c4595fef0
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2024-09-09 23:44:42 +0600

    ui initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="full flex flex-col">
+      <div className="flex-none h-10 bg-gray-400"></div>
+      <div className="flex-1">
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel>One</ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel>Two</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-3xl font-bold underline">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="flex-none h-10 bg-gray-900"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
