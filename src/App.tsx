aa9eac3205a6b06464ff09fc454e41a7add9f900
Tamim Ehsan <tamimehsan99@gmail.com>
2024-09-11 20:52:28 +0600

    feat: restructure preview renderer

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { ModeToggle } from "./components/mode-toggle";
 import { Button } from "./components/ui/button";
 import "./App.css";
 import { Textarea } from "@/components/ui/textarea"
+import Preview from "./Preview";
 //import { TypstDocument } from "@myriaddreamin/typst.react/TypstDocument";
 
 // TypstDocument.setWasmModuleInitOptions({
@@ -21,82 +22,6 @@ import { Textarea } from "@/components/ui/textarea"
 function App() {
   
   const [content, setContent] = useState<string>();
-  const [inputValue, setInputValue] = useState('');
-  const contentDivRef = useRef<HTMLDivElement>(null);
-  const inputRef = useRef<HTMLTextAreaElement>(null);
-
-
-  const previewSvg = (mainContent: string) => {
-    console.log('previewSvg', mainContent); 
-    if (typeof $typst !== 'undefined') {
-      $typst.svg({ mainContent }).then((svg: string) => {
-        if (contentDivRef.current) {
-          console.log(`rendered! SvgElement { len: ${svg.length} }`);
-          //console.log(svg);
-          contentDivRef.current.innerHTML = svg;
-
-          const svgElem = contentDivRef.current.firstElementChild as SVGElement;
-          const width = Number.parseFloat(svgElem.getAttribute('width') || '0');
-          const height = Number.parseFloat(svgElem.getAttribute('height') || '0');
-          const cw = document.body.clientWidth - 40;
-          svgElem.setAttribute('width', cw.toString());
-          svgElem.setAttribute('height', ((height * cw) / width).toString());
-          console.log(`width: ${width}, height: ${height}, cw: ${cw}`);
-        }else{
-          console.log('contentDivRef.current is null');
-        }
-      });
-    }
-  };
-
-  const exportPdf = (mainContent: string) => {
-    console.log('exportPdf', mainContent);
-    if (typeof $typst !== 'undefined') {
-      $typst.pdf({ mainContent }).then((pdfData: BlobPart) => {
-        const pdfFile = new Blob([pdfData], { type: 'application/pdf' });
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(pdfFile);
-        link.target = '_blank';
-        link.click();
-        URL.revokeObjectURL(link.href);
-      });
-    }
-  };
-
-  const initializeTypst = () => {
-    //if (typeof $typst !== 'undefined') {
-      $typst.setCompilerInitOptions({
-        getModule: () =>
-          'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-web-compiler/pkg/typst_ts_web_compiler_bg.wasm',
-      });
-      $typst.setRendererInitOptions({
-        getModule: () =>
-          'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-renderer/pkg/typst_ts_renderer_bg.wasm',
-      });
-
-      if (inputRef.current) {
-        inputRef.current.oninput = () => {
-          if (inputRef.current) {
-            console.log(inputRef.current.value);
-            previewSvg(inputRef.current.value || '');
-            inputRef.current.style.height = '5px';
-            inputRef.current.style.height = `${inputRef.current.scrollHeight}px`;
-          }
-        };
-        previewSvg(inputRef.current.value);
-      }
-    }
-  //  };
-
-  const script = document.createElement('script'); 
-  script.type = 'module';
-  script.src = 'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst.ts/dist/esm/contrib/all-in-one-lite.bundle.js';
-  script.id = 'typst';
-  script.onload = initializeTypst;
-  document.body.appendChild(script);
-
-    
-
 
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -179,21 +104,11 @@ function App() {
         <div className="flex-1">
           <ResizablePanelGroup direction="horizontal">
             <ResizablePanel>
-              <Textarea 
-              placeholder="Type your message here." 
-              className="full" 
-              id="input" 
-              ref={inputRef}
-              value={inputValue}
-              onInput={(e) => {
-                setInputValue(e.currentTarget.value);
-              
-              }
-              }/>
+              <div> hello world!</div>
             </ResizablePanel>
             <ResizableHandle withHandle />
             <ResizablePanel>
-                <div id="content" ref={contentDivRef}></div>
+                <Preview />
             </ResizablePanel>
           </ResizablePanelGroup>
         </div>
