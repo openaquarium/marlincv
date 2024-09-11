853d02533be5360a872a61f7517f4b889ba67d7b
Hasan Masum <1805052@ugrad.cse.buet.ac.bd>
2024-09-11 17:00:53 +0600

    rendered unexpectedly

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,103 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Download, File, LayoutTemplate, Play, Save } from "lucide-react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "./components/mode-toggle";
 import { Button } from "./components/ui/button";
 import "./App.css";
-import Sample from "./Pdf";
+import { Textarea } from "@/components/ui/textarea"
+//import { TypstDocument } from "@myriaddreamin/typst.react/TypstDocument";
+
+// TypstDocument.setWasmModuleInitOptions({
+//   beforeBuild: [],
+//   getModule: () =>
+//     'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-web-compiler/pkg/typst_ts_web_compiler_bg.wasm',
+// });
 
 function App() {
   
-  const [content, setContent] = useState<string | null>(null);
+  const [content, setContent] = useState<string>();
+  const [inputValue, setInputValue] = useState('');
+  const contentDivRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
+
+  useEffect(() => {
+    const previewSvg = (mainContent: string) => {
+      console.log('previewSvg', mainContent);
+      if (typeof $typst !== 'undefined') {
+        $typst.svg({ mainContent }).then((svg: string) => {
+          if (contentDivRef.current) {
+            console.log(`rendered! SvgElement { len: ${svg.length} }`);
+            contentDivRef.current.innerHTML = svg;
+
+            const svgElem = contentDivRef.current.firstElementChild as SVGElement;
+            const width = Number.parseFloat(svgElem.getAttribute('width') || '0');
+            const height = Number.parseFloat(svgElem.getAttribute('height') || '0');
+            const cw = document.body.clientWidth - 40;
+            svgElem.setAttribute('width', cw.toString());
+            svgElem.setAttribute('height', ((height * cw) / width).toString());
+            console.log(`width: ${width}, height: ${height}, cw: ${cw}`);
+          }else{
+            console.log('contentDivRef.current is null');
+          }
+        });
+      }
+    };
+
+    const exportPdf = (mainContent: string) => {
+      if (typeof $typst !== 'undefined') {
+        $typst.pdf({ mainContent }).then((pdfData: BlobPart) => {
+          const pdfFile = new Blob([pdfData], { type: 'application/pdf' });
+          const link = document.createElement('a');
+          link.href = URL.createObjectURL(pdfFile);
+          link.target = '_blank';
+          link.click();
+          URL.revokeObjectURL(link.href);
+        });
+      }
+    };
+
+    const initializeTypst = () => {
+      //if (typeof $typst !== 'undefined') {
+        $typst.setCompilerInitOptions({
+          getModule: () =>
+            'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-web-compiler/pkg/typst_ts_web_compiler_bg.wasm',
+        });
+        $typst.setRendererInitOptions({
+          getModule: () =>
+            'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-renderer/pkg/typst_ts_renderer_bg.wasm',
+        });
+
+        if (inputRef.current) {
+          inputRef.current.oninput = () => {
+            if (inputRef.current) {
+              console.log(inputRef.current.value);
+              previewSvg(inputRef.current.value || '');
+              inputRef.current.style.height = '5px';
+              inputRef.current.style.height = `${inputRef.current.scrollHeight}px`;
+            }
+          };
+          previewSvg(inputRef.current.value);
+        }
+      }
+    //  };
+
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = 'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst.ts/dist/esm/contrib/all-in-one-lite.bundle.js';
+    script.id = 'typst';
+    script.onload = initializeTypst;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -22,6 +108,7 @@ function App() {
       reader.onload = (e) => {
         const text = e.target?.result as string;
         setContent(text);
+        console.log(text);
       };
       reader.readAsText(file);
     }
@@ -93,10 +180,22 @@ function App() {
         </div>
         <div className="flex-1">
           <ResizablePanelGroup direction="horizontal">
-            <ResizablePanel>{content}</ResizablePanel>
+            <ResizablePanel>
+              <Textarea 
+              placeholder="Type your message here." 
+              className="full" 
+              id="input" 
+              ref={inputRef}
+              value={inputValue}
+              onInput={(e) => {
+                setInputValue(e.currentTarget.value);
+              
+              }
+              }/>
+            </ResizablePanel>
             <ResizableHandle withHandle />
             <ResizablePanel>
-              <Sample />
+                <div id="content" ref={contentDivRef}></div>
             </ResizablePanel>
           </ResizablePanelGroup>
         </div>
